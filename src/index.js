840260b3ef015a7ebab973d96361cad58b4d1e1c
Michael <kllmmc23@gmail.com>
2020-05-22 19:34:58 -0600

    error handling

--- src/index.js
+++ src/index.js
@@ -35,13 +35,13 @@ class App extends React.Component {
   }
   //render()---required
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.erroMessage}
-      </div>
-    );
+    if (this.state.erroMessage && !this.state.lat) {
+      return <div>Error: {this.state.erroMessage}</div>;
+    }
+    if (!this.state.erroMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
   }
 }
 
