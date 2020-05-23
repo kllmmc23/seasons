b308f1053c392a43b4ee4cde4f6aded002aaf61e
Michael <kllmmc23@gmail.com>
2020-05-22 19:27:32 -0600

    considitonal-renderin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>Hello</div>;
-};
+const rootElement = document.getElementById("root");
 
-ReactDOM.render(<App />,document.getElementById("root"));
+// const App = () => {
+//   const [lat, setLat] = useState();
+
+//   console.log(lat);
+
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => {
+//       setLat(window.navigator.geolocation.getCurrentPosition());
+
+//     },
+//     (err) => console.log(err)
+//   );
+//   return <div>Latitude: {lat}</div>;
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, erroMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.state({ erroMessage: err.message });
+      }
+    );
+  }
+  //render()---required
+  render() {
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.erroMessage}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, rootElement);
