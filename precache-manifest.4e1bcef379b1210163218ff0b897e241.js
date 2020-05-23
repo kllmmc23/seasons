5b769074c03dac12996ec5695058e7597c9dab3e
Michael <kllmmc23@gmail.com>
2020-05-22 23:31:00 -0600

    Updates

---
+++ precache-manifest.4e1bcef379b1210163218ff0b897e241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1219b5888a8f7047b322b4b990046ecc",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "2d7c39e97767ac5b2cf1",
+    "url": "/seasons/static/css/main.0b83e14d.chunk.css"
+  },
+  {
+    "revision": "a1fe034d8847b2c1a104",
+    "url": "/seasons/static/js/2.f892ca35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/seasons/static/js/2.f892ca35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d7c39e97767ac5b2cf1",
+    "url": "/seasons/static/js/main.949bc7d3.chunk.js"
+  },
+  {
+    "revision": "5f8b8b387793381e1aa0",
+    "url": "/seasons/static/js/runtime-main.683bff03.js"
+  }
+]);
