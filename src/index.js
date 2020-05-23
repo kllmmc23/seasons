02bfd560ff83586c6a162dd10fa3b2173f8aed1c
Michael <kllmmc23@gmail.com>
2020-05-22 19:53:58 -0600

    passing state as props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from './SeasonDisplay'
 
 const rootElement = document.getElementById("root");
 
+class App extends React.Component {
+  state = { lat: null, erroMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.state({ erroMessage: err.message })
+    );
+  }
+  render() {
+    if (this.state.erroMessage && !this.state.lat) {
+      return <div>Error: {this.state.erroMessage}</div>;
+    }
+    if (!this.state.erroMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat}/>;
+    }
+    return <div>Loading!</div>;
+  }
+}
 // const App = () => {
 //   const [lat, setLat] = useState();
 
@@ -17,32 +37,4 @@ const rootElement = document.getElementById("root");
 //   );
 //   return <div>Latitude: {lat}</div>;
 // };
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, erroMessage: "" };
-
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.state({ erroMessage: err.message });
-      }
-    );
-  }
-  //render()---required
-  render() {
-    if (this.state.erroMessage && !this.state.lat) {
-      return <div>Error: {this.state.erroMessage}</div>;
-    }
-    if (!this.state.erroMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    }
-    return <div>Loading!</div>;
-  }
-}
-
 ReactDOM.render(<App />, rootElement);
