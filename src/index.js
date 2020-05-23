f1412025ff27baaac20ec7456bda8ea1ae7ae7be
Michael <kllmmc23@gmail.com>
2020-05-22 22:01:53 -0600

    final

--- src/index.js
+++ src/index.js
@@ -1,40 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from './SeasonDisplay'
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 const rootElement = document.getElementById("root");
 
 class App extends React.Component {
-  state = { lat: null, erroMessage: "" };
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.state({ erroMessage: err.message })
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="Please wait a sec" />;
+  }
+
   render() {
-    if (this.state.erroMessage && !this.state.lat) {
-      return <div>Error: {this.state.erroMessage}</div>;
-    }
-    if (!this.state.erroMessage && this.state.lat) {
-        return <SeasonDisplay lat={this.state.lat}/>;
-    }
-    return <div>Loading!</div>;
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
-// const App = () => {
-//   const [lat, setLat] = useState();
-
-//   console.log(lat);
-
-//   window.navigator.geolocation.getCurrentPosition(
-//     (position) => {
-//       setLat(window.navigator.geolocation.getCurrentPosition());
-
-//     },
-//     (err) => console.log(err)
-//   );
-//   return <div>Latitude: {lat}</div>;
-// };
 ReactDOM.render(<App />, rootElement);
